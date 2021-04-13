c697c7a0d5dc27591300ee2a466c881099cebb79
jamespang14 <75464908+jamespang14@users.noreply.github.com>
2021-04-13 23:40:49 +0930

    commit

--- clock.py
+++ clock.py
@@ -7,6 +7,20 @@ import stock as st
 #update all stock datas in the folder within time interval
 sched = BlockingScheduler()
 
+def update_data():
+    signal = 0
+    for filename in glob.glob("./stock_data/*.csv"):
+        signal = signal + 1
+        stock_name = filename
+        stock_name=stock_name.replace('./stock_data/', '')
+        stock_name=stock_name.replace('.csv', '')
+        try:
+            st.get_current_stock_history(stock_name)
+        except:
+            pass
+        print("Stock: "+str(signal))
+    print("Stock history updated")
+
 @sched.scheduled_job('interval', minutes=1440)
 def download_stock():
     signal = 0
