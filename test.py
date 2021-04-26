db3775f9099a33d2a42637c19064a107d6376050
jamespang14 <75464908+jamespang14@users.noreply.github.com>
2021-04-26 20:02:17 +0930

    Update

--- test.py
+++ test.py
@@ -1,23 +1,19 @@
 from selenium import webdriver
 import pandas as pd
 
+driver = webdriver.Safari()
+#driver.implicitly_wait(30)
 
-driver = webdriver.Chrome('./chromedriver')
-driver.get("https://www2.asx.com.au/markets/trade-our-cash-market/announcements.3DP")
-
-html = driver.page_source
-
-tables = pd.read_html(html)
-data = tables[1]
-
-driver.close()
+driver.get('https://www2.asx.com.au/markets/trade-our-cash-market/announcements.3DP')
+df=pd.read_html(driver.find_element_by_id("data-v-4784e3fa").get_attribute('outerHTML'))[0]
+print(df)
 
 # import requests
 # import pandas as pd
 
 # url = 'https://www2.asx.com.au/markets/trade-our-cash-market/announcements.3DP'
 # html = requests.get(url).content
-# df_list = pd.read_html(str(soup.find_all('table')[0])
+# df_list = pd.read_html(html)
 # df = df_list[-1]
 # print(df)
-# # #df.to_csv('my data.csv')
+# #df.to_csv('my data.csv')
