a0c65762efcedf4194a93f46ad128011dbb6064a
jamespang14 <75464908+jamespang14@users.noreply.github.com>
2021-05-21 12:00:00 +0930

    Update application.py

--- application.py
+++ application.py
@@ -398,7 +398,7 @@ def stockmon(stockmon_filter):
                                 "CV":int(float(rows[360][2])*int(rows[360][6])),
                                 }
                             if element['change_high'] > 0 and element['change_open'] > 0 and element['change_low'] > 0 and element['change_close'] > 0 and element['change_volume'] > 0 and element['Volume']>element['vol6m'] and element['Volume']>element['vol12m']:
-                                update_date = str(rows[359][0])
+                                update_date = str(rows[360][0])
                                 if stockmon_filter == "3days":
                                     if element['change_close_1'] > 0 and element['change_close_2'] > 0:
                                         monitorList.append(element)
